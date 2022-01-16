b66c262d76cf9f2be8a049eca3290d1ea4486237
viekkar <viekkar@gmail.com>
2022-01-16 14:54:35 +0530

    2nd commit

--- src/StringsPrgms/SortArray1.java
+++ src/StringsPrgms/SortArray1.java
@@ -1,9 +1,16 @@
 package StringsPrgms;
 
+import java.util.Arrays;
+
 public class SortArray1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] exmp= {12,55,84,656,6,4,};
+		System.out.println("Before sorting"+Arrays.toString(exmp));
+		Arrays.sort(exmp);
+		System.out.println("after sorting"+Arrays.toString(exmp));
+	
+		
 
 	}
 
